e61f8fd4da976f14baeca67f475957e0ce847144
Karen Kraft <kraftkj@comcast.net>
2018-07-23 20:09:58 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,9 +1,10 @@
 
 # Code your methods below
-def the_dance1
+def the_dance
   puts lift right foot
   puts right foot to the grouond 
   puts small Skip-Step
 end
 
-def the_dance1
+
+def the_dance
